8461049b2758e550f1285b1c3f404239a3c667e0
noel <noelperal@yahoo.es>
2019-06-05 00:30:59 +0200

    -using internal http request API from wordpress instead CURL: https://developer.wordpress.org/reference/classes/WP_Http/request/

--- githubin/githubin.php
+++ githubin/githubin.php
@@ -29,7 +29,6 @@ function githubin($atts)
 		$content_githubin=get_cached_githubin($atts["id"]);
 	}
 	
-	
 	if( !isset($content_githubin) || (isset($content_githubin) && $content_githubin=="false") )	//si no lo cogió de la cache o lo cogió pero falló, lo genera entero
 	{
 		if(isset($atts["url"]) && $atts["url"]!="PUT_GITHUB_URL_HERE")
@@ -136,12 +135,12 @@ function githubin($atts)
 			}
 		}
 
-
 	//obtenemos el contenido
 		
 		//$useragent = "Mozilla/5.0 (Linux; Android 4.4.".rand(1,4)."; C2105 Build/15.3.A.1.14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.93 Mobile Safari/537.36"; //android 4.4.*
 		$useragent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A5370a Safari/604.1"; //iphone os11
 
+		/* curl (not using anymore)
 		 $ch = curl_init($url);
 		 curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
 		 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -149,6 +148,13 @@ function githubin($atts)
 		 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		 $content_githubin = curl_exec($ch);
 		 curl_close($ch);
+		*/
+		
+		//using wordpress request internal API (https://developer.wordpress.org/reference/classes/WP_Http/request/)
+		$args_rr = array('user-agent'=> apply_filters( 'http_headers_useragent', $useragent , $url ));
+
+		$resp = wp_remote_request( $url, $args_rr );
+		$content_githubin=$resp['body'];
 
 	 //limpiamos el header
 		 switch($type)
